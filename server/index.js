6d0a64d4c567ff82a5215b7b77ce879024b09b0f
DaGo85 <davidgoe85@gmail.com>
2022-05-06 16:11:34 +0200

    added dependencies

---
+++ server/index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+
+const corsOptions = {
+  origin: "http://localhost:5001",
+};
+
+app.use(cors(corsOptions));
+// parse requests of content-type - application/json
+app.use(express.json());
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }));
+
+// simple route
+app.get("/", (req, res) => {
+  res.json({ message: "Test" });
+});
+
+// set port, listen for requests
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
