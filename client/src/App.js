72c7a784b4680a54ae4c88399e33b795c5eadc52
DaGo85 <davidgoe85@gmail.com>
2022-05-08 17:37:29 +0200

    added first auth logic

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,48 @@
 import { Outlet } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useEffect } from "react";
+
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import { useAuthContext } from "./utility/AutContext";
 
 import NavBar from "./components/navbar/NavBar";
+import Home from "./pages/home/Home";
+import Recipes from "./pages/recipes/Recipes";
+import SingleRecipe from "./pages/singlerecipe/SingleRecipe";
+import NotFound from "./pages/notfound/NotFound";
 
 function App() {
+  const auth = getAuth();
+  const { userData, setUserData } = useAuthContext();
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        // User is signed in, see docs for a list of available properties
+        // https://firebase.google.com/docs/reference/js/firebase.User
+        const uid = user.uid;
+        const email = user.email;
+
+        const newUser = { uid, email };
+
+        setUserData(newUser);
+      }
+    });
+  }, [auth]);
+
   return (
-    <>
+    <BrowserRouter>
       <NavBar />
       <Outlet />
-    </>
+      <Routes>
+        <Route path="/" element={<Home />}>
+          <Route index element={<Home />} />
+          <Route path="recipes" element={<Recipes />} />
+          <Route path="recipe:singlerecipeId" element={<SingleRecipe />} />
+          <Route path="*" element={<NotFound />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- client/src/index.js
+++ client/src/index.js
@@ -2,28 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import Home from "./pages/home/Home";
-import Recipes from "./pages/recipes/Recipes";
-import SingleRecipe from "./pages/singlerecipe/SingleRecipe";
-import NotFound from "./pages/notfound/NotFound";
 import { ThemeProvider } from "./utility/darkmode/ThemeContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <ThemeProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<App />}>
-            <Route index element={<Home />} />
-            <Route path="recipes" element={<Recipes />} />
-            <Route path="recipe:singlerecipeId" element={<SingleRecipe />} />
-            <Route path="*" element={<NotFound />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
+      <App />
     </ThemeProvider>
   </React.StrictMode>
 );
