7dff6d7e79096c32a7f1fd36a8731d9ee68eee7e
DaGo85 <davidgoe85@gmail.com>
2022-05-10 15:59:23 +0200

    added pagination and other functionality for recipes page

--- client/src/pages/recipes/Recipes.js
+++ client/src/pages/recipes/Recipes.js
@@ -1,5 +1,39 @@
+import { useMemo, useState } from "react";
+import Pagination from "../../components/pagination/Pagination";
+import RecipeTable from "./components/RecipeTable";
+
+const PageSize = 9;
+
 function Recipes() {
-  return <>Recipes</>;
+  const [currentPage, setCurrentPage] = useState(1);
+  const [filteredRecipes, setFilteredRecipes] = useState([
+    1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
+  ]);
+
+  const currentGridData = useMemo(() => {
+    const firstPageIndex = (currentPage - 1) * PageSize;
+    const lastPageIndex = firstPageIndex + PageSize;
+    return filteredRecipes.slice(firstPageIndex, lastPageIndex);
+  }, [currentPage, filteredRecipes]);
+
+  return (
+    <main className="background-setup">
+      <section className="">Filter Section</section>
+      <Pagination
+        currentPage={currentPage}
+        totalCount={filteredRecipes.length}
+        pageSize={PageSize}
+        onPageChange={(page) => setCurrentPage(page)}
+      />
+      <RecipeTable currentGridData={currentGridData} />
+      <Pagination
+        currentPage={currentPage}
+        totalCount={filteredRecipes.length}
+        pageSize={PageSize}
+        onPageChange={(page) => setCurrentPage(page)}
+      />
+    </main>
+  );
 }
 
 export default Recipes;
---
+++ client/src/pages/recipes/components/RecipeTable.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function RecipeTable() {
+  return <div>RecipeTable</div>;
+}
+
+export default RecipeTable;
