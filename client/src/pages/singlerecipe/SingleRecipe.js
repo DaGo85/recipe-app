8187ba98a5ef2d883e2dbbbcb38e6f0ad5e1cdbe
DaGo85 <davidgoe85@gmail.com>
2022-05-10 16:16:09 +0200

    added structure for singlerecipe

--- client/src/pages/singlerecipe/SingleRecipe.js
+++ client/src/pages/singlerecipe/SingleRecipe.js
@@ -1,5 +1,23 @@
 function SingleRecipe() {
-  return <>SingleRecipe</>;
+  return (
+    <main className="background-setup">
+      <h1>Name of recipe</h1>
+      <ul>
+        <li>tags</li>
+      </ul>
+      <ul>
+        <li>ingredients</li>
+      </ul>
+      <section>
+        <p>description</p>
+      </section>
+      <button>delete</button>
+      <button>edit</button>
+      <div>votecounter</div>
+      <button>upvote if logged in</button>
+      <section>commentsection if logged in?</section>
+    </main>
+  );
 }
 
 export default SingleRecipe;
