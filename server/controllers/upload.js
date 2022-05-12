31f4cb99061450d990e17a80088e7a6916e5d382
DaGo85 <davidgoe85@gmail.com>
2022-05-12 16:24:08 +0200

    fixed another bug regarding uploading images

--- server/controllers/upload.js
+++ server/controllers/upload.js
@@ -5,21 +5,17 @@ const Image = db.images;
 
 const uploadFiles = async (req, res) => {
   try {
-    console.log("upload" + req.body.url);
-    if (req.body.url == undefined) {
+    console.log("upload" + req.file);
+    if (req.file == undefined) {
       return res.send(`You must select a file.`);
     }
     Image.create({
       type: req.body.mimetype,
       name: req.body.originalname,
-      data: fs.readFileSync(
-        __basedir + "/resources/static/assets/uploads/" + req.body.url
-      ),
+      data: fs.readFileSync(__basedir + "/uploads/" + req.body.url),
     }).then((image) => {
-      fs.writeFileSync(
-        __basedir + "/resources/static/assets/tmp/" + image.name,
-        image.data
-      );
+      fs.writeFileSync(__basedir + "/tmp/" + image.name, image.data);
+      console.log("test");
       return res.send(`File has been uploaded.`);
     });
   } catch (error) {
