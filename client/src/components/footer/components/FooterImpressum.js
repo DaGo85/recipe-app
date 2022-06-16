7ee7504047ef2ea2333ad167285a905491e7d71f
DaGo85 <davidgoe85@gmail.com>
2022-06-16 14:46:59 +0200

    minor html tag fix

--- client/src/components/footer/components/FooterImpressum.js
+++ client/src/components/footer/components/FooterImpressum.js
@@ -2,25 +2,25 @@ import FooterSocialIcons from "./FooterSocialIcons";
 
 function FooterImpressum() {
   return (
-    <address className="flex flex-col md:flex-row gap-6">
+    <section className="flex flex-col md:flex-row gap-6">
       <span className="flex flex-col gap-4 w-full">
         <h3>According to § 5 TMG:</h3>
         <hr className="hr-setup" />
-        <p>
+        <address>
           D. Goergens
           <br />
           Dorstener Strasse 534
           <br />
           46119 Oberhausen
-        </p>
+        </address>
       </span>
       <span className="flex flex-col gap-4 w-full">
         <h3>Contact:</h3>
         <hr className="hr-setup" />
-        <p>E-Mail: d.goergens@gmail.com</p>
+        <address>E-Mail: d.goergens@gmail.com</address>
         <FooterSocialIcons />
       </span>
-    </address>
+    </section>
   );
 }
 
